36e887c63f5e2abeddc15b6e3faa2445aa352659
Luca <lucatorroni@hotmail.com>
2015-04-19 00:39:20 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -12,8 +12,15 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
 }
 
 // put new methods here
+
+-(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    [character flap];
+}
 
 @end
