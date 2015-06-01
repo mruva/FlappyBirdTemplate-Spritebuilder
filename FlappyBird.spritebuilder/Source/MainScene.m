c4ebaead7cb559491eedaee2cabe5236b532720e
Luca <lucatorroni@hotmail.com>
2015-06-01 23:51:42 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -21,7 +21,7 @@
 @property (nonatomic, readwrite, unsafe_unretained) CCNode *child;
 
 +(id) pointWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
--(id) initWithCGPOint:(CGPOint)point offset:(CGPoint)offset;
+-(id) initWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
 @end
 
 @implementation MainScene {
